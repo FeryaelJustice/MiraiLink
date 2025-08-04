7fcc09e73dc2c5c95a3c0e6a9f6f8f3e3b9a18c1
FeryaelJustice <nano9gs@hotmail.es>
2025-08-04 23:49:50 +0200

    Refactor: Update default value for TwoFactorAuthInfo.enabled
    This commit changes the default value of the `enabled` property in the `TwoFactorAuthInfo` data class from `true` to `false`.

--- app/src/main/java/com/feryaeljustice/mirailink/domain/model/auth/TwoFactorAuthInfo.kt
+++ app/src/main/java/com/feryaeljustice/mirailink/domain/model/auth/TwoFactorAuthInfo.kt
@@ -1,7 +1,7 @@
 package com.feryaeljustice.mirailink.domain.model.auth
 
 data class TwoFactorAuthInfo(
-    val enabled: Boolean? = true,
+    val enabled: Boolean? = false,
     val otpAuthUrl: String? = null,
     val baseCode: String? = null,
     val recoveryCodes: List<String>? = emptyList()
