f1c29a05db45e6c35411f7a0b53c126bbadd53ac
FeryaelJustice <nano9gs@hotmail.es>
2025-11-03 00:54:22 +0100

    Chore: Bump app version to 1.2.0
    This commit increments the application's version information.
    
    - `versionCode` is updated from 17 to 18.
    - `versionName` is updated from "1.1.6" to "1.2.0".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
 
         minSdk = 26
         targetSdk = 36
-        versionCode = 17
-        versionName = "1.1.6"
+        versionCode = 18
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "com.feryaeljustice.mirailink.CustomHiltTestRunner"
     }
